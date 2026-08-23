8285788b7ec16340ab1f64996d7fcf426d7b5d7e
Francisco Pascoal <fpascoal1996@gmail.com>
2026-08-23 21:07:25 +0100

    Make a function to generate context embedding

--- python/generate_contextualized_embeddings.py
+++ python/generate_contextualized_embeddings.py
@@ -1,30 +1,45 @@
 """Generate contextualized word embeddings
         based on https://learning.oreilly.com/library/view/hands-on-large-language/9781098150952/ch02.html#a_language_model_holds_embeddings_for_t
 """
-
 from transformers import AutoModel, AutoTokenizer
 
-# Load a tokenizer
-tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-base")
+def context_embedding(sentence, tokenizer, model):
+    # Load a tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer)
 
-# Load a language model
-model = AutoModel.from_pretrained("microsoft/deberta-v3-xsmall")
+    # Load a language model
+    model = AutoModel.from_pretrained(model)
 
-# Tokenize the sentence
-tokens = tokenizer('Hello world', return_tensors='pt')
+    # Tokenize the sentence
+    tokens = tokenizer(sentence, return_tensors='pt')
 
-# Process the tokens
-output = model(**tokens)[0]
+    # Process the tokens
+    output = model(**tokens)[0]
 
-output.shape
 
-# inspect the vectorrs
-for token in tokens['input_ids'][0]:
-    print(tokenizer.decode(token))
+context_embedding(tokenizer = "microsoft/deberta-base",
+                  model =  "microsoft/deberta-v3-xsmall",
+                  sentence = "For example, this sentence :)")
 
-# the representation used by the LLM
-print(output)
 
-"""to do:
-- function to create context embeddings
-- compare different examples"""
+if __name__ == "__main__":
+    # Load a tokenizer
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-base")
+
+    # Load a language model
+    model = AutoModel.from_pretrained("microsoft/deberta-v3-xsmall")
+
+    # Tokenize the sentence
+    tokens = tokenizer('Hello world', return_tensors='pt')
+
+    # Process the tokens
+    output = model(**tokens)[0]
+
+    output.shape
+
+    # inspect the vectorrs
+    for token in tokens['input_ids'][0]:
+        print(tokenizer.decode(token))
+
+    # the representation used by the LLM
+    print(output)
