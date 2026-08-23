77f292931965392ba0006dc99588767a4104210b
Francisco Pascoal <fpascoal1996@gmail.com>
2026-08-23 15:31:53 +0100

    Code to import phi-3-4k

---
+++ load_models.py
@@ -0,0 +1,24 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+# Load model and tokenizer
+model = AutoModelForCausalLM.from_pretrained(
+        "microsoft/Phi-3-mini-4k-instruct",
+        device_map = "cuda",
+        torch_dtype = "auto",
+        trust_remote_code = True,
+        attn_implementation = "eager"
+    )
+
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
+
+
+# create a pipeline
+generator = pipeline(
+        "text-generation",
+        model = model,
+        tokenizer = tokenizer,
+        return_full_text = False,
+        max_new_tokens = 500,
+        do_sample = False
+    )
